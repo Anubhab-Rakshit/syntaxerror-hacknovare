6101b46cc2e6463bf11ee49c272afc5efb99e781
Anubhab Rakshit <95529010+Anubhab-Rakshit@users.noreply.github.com>
2025-01-12 11:18:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const multer = require("multer");
 const collection = require("./mongodb");
+const session = require("express-session");
 
 const app = express();
 const port = 3000;
